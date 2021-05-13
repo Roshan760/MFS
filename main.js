aec004a0e2c98167082491e07feee7d21c5601c1
Roshan760 <roshan3.hs@gmail.com>
2021-05-13 06:24:28 +0000

    Version 1.0.8 update

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const { autoUpdater } = require('electron-updater');
       }
     })
     console.log("started",mainWindow)
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/build/index.html`);
     // mainWindow.loadFile('.src/index.html');
     // mainWindow.loadFile('./dist/electron-demo/index.html')
     // mainWindow.loadURL(
