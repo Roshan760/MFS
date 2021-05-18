34b12b4690e21595103d2ecefc2b13a5b8262d47
Roshan760 <roshan3.hs@gmail.com>
2021-05-18 09:15:28 +0000

    Version 1.0.12 update

--- src/assets/generate.js
+++ src/assets/generate.js
@@ -13,16 +13,22 @@ function myCode(){
     });
 }
 function launchCode(){
-  //alert("invoke function");
-  var exec = require('child_process').exec, child;
-  console.log('calling java');
-  //console.log(exec.caller);
-  //const childPorcess = exec('java -jar C:\\Node_Trails\\hello-world\\invoke-jar.jar "Jar is invoked by Node js"', function(err, stdout, stderr) {
-  child = exec('java -jar  ../../../jarfiles/Sync_Service.jar',
-  function(err, stdout, stderr) {
-      if (err) {
-          console.log(err)
-      }
-      console.log(stdout)
-  });
-}
+    //alert("invoke function");
+    var exec = require('child_process').exec, child;
+    console.log('calling java');
+    console.log("process",process.cwd())
+    console.log("dir",__dirname)
+    console.log(process.resourcesPath)
+    //console.log(exec.caller);
+    //const childPorcess = exec('java -jar C:\\Node_Trails\\hello-world\\invoke-jar.jar "Jar is invoked by Node js"', function(err, stdout, stderr) {
+   // child = exec(`java -jar ${process.cwd()}\\resources\\app.asar\\src\\assets\\Sync_Service.jar`,
+  var path=`${process.resourcesPath}\\app.asar.unpacked\\src\\assets\\Sync_Service.jar`
+  console.log("path :",path)
+   child = exec(`java -jar ${path}`,
+    function(err, stdout, stderr) {
+        if (err) {
+            console.log(err)
+        }
+        console.log(stdout)
+    });
+  }
